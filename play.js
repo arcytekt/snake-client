4f88c0faa1f93d94b102d714761f7431f32868f0
Ethan Pelensky <ethan@arcytekt.com>
2024-01-18 09:28:51 -0700

    updated play.js using exports for setupInput.

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 const connection = connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // Ctrl+C input = '\u0003'
-    console.log("Exiting game.");
-    process.exit();
-  }
-};
-
 setupInput();
+
+// const { myNumber, myString, myFunction } = require("./fileA");
