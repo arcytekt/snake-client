7f58c9da84c83ad5d36b5999a96c8f587f65929b
Ethan Pelensky <ethan@arcytekt.com>
2024-01-18 09:52:14 -0700

    final, running version of snek which accepts wasd movement!

--- input.js
+++ input.js
@@ -1,9 +1,12 @@
-const setupInput = function () {
+let conn;
+
+const setupInput = function (connection) {
+  conn = connection;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key, conn));
   return stdin;
 };
 
@@ -11,6 +14,14 @@ const handleUserInput = function (key) {
   if (key === '\u0003') { // Ctrl+C input = '\u0003'
     console.log("Exiting game.");
     process.exit();
+  } else if (key === 'w') {
+    conn.write("Move: up");
+  } else if (key === 'a') {
+    conn.write("Move: left");
+  } else if (key === 's') {
+    conn.write("Move: down");
+  } else if (key === 'd') {
+    conn.write("Move: right");
   }
 };
 
--- play.js
+++ play.js
@@ -2,8 +2,7 @@ const connect = require("./client");
 const setupInput = require("./input");
 
 console.log("Connecting ...");
+
 const connection = connect();
 
-setupInput();
-
-// const { myNumber, myString, myFunction } = require("./fileA");
+setupInput(connection);
