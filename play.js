249602cd9cd041fd85f39265b3179a71273cdbe2
Ethan Pelensky <ethan@arcytekt.com>
2024-01-18 09:10:42 -0700

    updated snek with handleUserInput & setupInput.

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const connect = require("./client");
 
 console.log("Connecting ...");
 const connection = connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // Ctrl+C input = '\u0003'
+    console.log("Exiting game.");
+    process.exit();
+  }
+};
+
+setupInput();
