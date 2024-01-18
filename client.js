0a001a894d233db86831ed833c5f5957eab7d997
Ethan Pelensky <ethan@arcytekt.com>
2024-01-18 08:45:33 -0700

    updated version of snek with hard coded upward movement. Currently commented out.

--- client.js
+++ client.js
@@ -16,6 +16,7 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
     conn.write("Name: eet");
+    //conn.write("Move: up");
   });
 
   return conn;
