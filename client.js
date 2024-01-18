c5510b3cb26c533a93b99edfcf7af593116296e8
Ethan Pelensky <ethan@arcytekt.com>
2024-01-18 08:41:03 -0700

    updated version of client.js which adds my name to the game!

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
-    port: 50541,
+    port: 50541
   });
 
   conn.setEncoding("utf8");
@@ -11,6 +11,11 @@ const connect = function () {
   // event listener for data
   conn.on("data", (data) => {
     console.log("Server: ", data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: eet");
   });
 
   return conn;
